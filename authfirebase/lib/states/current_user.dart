fde9b1b9477d921df6b72df319504b4b0a3c2cc1
Muhammad Tawil <muhammad.tawil@hotmail.com>
2022-10-11 12:42:13 +0300

    add google sign in auth

--- authfirebase/lib/states/current_user.dart
+++ authfirebase/lib/states/current_user.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class CurrentUser with ChangeNotifier {
   String? _uid;
@@ -30,6 +31,33 @@ class CurrentUser with ChangeNotifier {
 
     try {
       await _auth.signInWithEmailAndPassword(email: email, password: password);
+      _uid = FirebaseAuth.instance.currentUser!.uid;
+      _email = FirebaseAuth.instance.currentUser!.email;
+      retVal = 'success';
+    } catch (e) {
+      retVal = e.toString();
+    }
+
+    return retVal;
+  }
+
+  Future<String> loginUserWithGoogle() async {
+    String retVal = 'error';
+    // GoogleSignIn googleSignIn = GoogleSignIn(
+    //   scopes: [
+    //     'email',
+    //     'https://www.googleapis.com/auth/contacts.readonly',
+    //   ],
+    // );
+    try {
+      GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+      final GoogleSignInAuthentication? googleAuth =
+          await googleUser?.authentication;
+      final credential =
+          GoogleAuthProvider.credential(idToken: googleAuth?.idToken);
+
+      await _auth.signInWithCredential(credential);
       _uid = FirebaseAuth.instance.currentUser!.uid;
       _email = FirebaseAuth.instance.currentUser!.email;
       retVal = 'success';
